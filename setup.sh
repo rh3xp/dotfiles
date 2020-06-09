9c04d5f1633fcd38cbf6219cdbe06a31ca7851e4
Parth Krishna <thenerdsuperuser@gmail.com>
2020-06-09 06:44:18 +0530

    Install Teamviewver on Arch

---
+++ setup.sh
@@ -0,0 +1,24 @@
+# TeamViewver
+
+
+sudo pacman -Sy
+
+sudo pacman -S git
+
+git clone https://aur.archlinux.org/teamviewer.git
+
+cd teamviewer/
+
+makepkg -s
+
+sudo pacman -Sy
+
+sudo pacman -U teamviewer*.pkg.tar.xz
+
+
+sudo systemctl status teamviewerd
+
+sudo systemctl start teamviewerd
+
+sudo systemctl enable teamviewerd
+
